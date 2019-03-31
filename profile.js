b192f9d372e271dd55302255c976af51f582ce06
Iqbal20 <48879334+Iqbal20@users.noreply.github.com>
2019-03-31 14:11:29 +0600

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+if(!isset($_SESSION["uid"])){
+	header("location:index.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>iLoveCraft</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<style>
+			@media screen and (max-width:480px){
+				#search{width:80%;}
+				#search_btn{width:30%;float:right;margin-top:-32px;margin-right:10px;}
+			}
+		</style>
+	</head>
+
+</html>
